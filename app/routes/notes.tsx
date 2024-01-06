28db04b50934a6d9fd3acc8caca1b048522a0af2
Alvaro Castro Cid <58068322+fidotheprince@users.noreply.github.com>
2024-01-06 17:37:17 +0000

    able to load data;

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,13 +1,40 @@
-import { ActionFunctionArgs, redirect } from '@remix-run/node';
+import NoteList, {links as noteListLinks} from '~/components/NoteList'; 
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+import { json, ActionFunctionArgs, redirect } from '@remix-run/node';
 import { getStoredNotes, storeNotes } from '~/data/notes';
+import { useLoaderData } from '@remix-run/react';
+
+// Note interface
+interface Note {
+    id: string;
+    title: string;
+    content: string;
+}
 
 export default function NotesPage() {
+
+    //useLoaderData hook is used to access the data returned from the loader function
+    //Note[] is an array of Note objects
+    const notes : Note[] = useLoaderData();
+    
     return (
         <main>
             <NewNote />
+            <NoteList notes={notes}/>
         </main>
      );
+}
+
+//load, fetch, the notes from the data store
+//return data is sent to the client, below are three ways to return data
+/*
+    1. return new Response(JSON.stringify({ notes }), {headers: {'Content-Type': 'application/json'}});
+    2. return json(notes);
+    3. return notes;
+*/
+export async function loader() {
+    const notes = await getStoredNotes();
+    return notes;
 }
 
 //action function is used to capture the form data and send it to the server
@@ -30,5 +57,5 @@ export async function action({ request }: ActionFunctionArgs) {
 
 //surface the styling links from the NewNote component
 export function links() {
-    return [...newNoteLinks()];
+    return [...newNoteLinks(), ...noteListLinks()];
 }
