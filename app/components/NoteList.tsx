5feb704b76ca7efc84747e916c11e69efdcee60f
Alvaro Castro Cid <58068322+fidotheprince@users.noreply.github.com>
2024-01-07 00:44:04 +0000

    added dynamic routes;

--- app/components/NoteList.tsx
+++ app/components/NoteList.tsx
@@ -1,4 +1,5 @@
 import styles from './NoteList.css';
+import { Link } from '@remix-run/react';
 
 // Note interface
 interface Note {
@@ -18,26 +19,29 @@ function NoteList({ notes }: NoteListProps) {
     <ul id="note-list">
       {notes.map((note, index) => (
         <li key={note.id} className="note">
-          <article>
-            <header>
-              <ul className="note-meta">
-                <li>#{index + 1}</li>
-                <li>
-                  <time dateTime={note.id}>
-                    {new Date(note.id).toLocaleDateString('en-US', {
-                      day: 'numeric',
-                      month: 'short',
-                      year: 'numeric',
-                      hour: '2-digit',
-                      minute: '2-digit',
-                    })}
-                  </time>
-                </li>
-              </ul>
-              <h2>{note.title}</h2>
-            </header>
-            <p>{note.content}</p>
-          </article>
+          {/*Appends to current active path notes/id */}
+          <Link to={`/note/${note.id}`}>
+            <article>
+              <header>
+                <ul className="note-meta">
+                  <li>#{index + 1}</li>
+                  <li>
+                    <time dateTime={note.id}>
+                      {new Date(note.id).toLocaleDateString('en-US', {
+                        day: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                      })}
+                    </time>
+                  </li>
+                </ul>
+                <h2>{note.title}</h2>
+              </header>
+              <p>{note.content}</p>
+            </article>
+          </Link>
         </li>
       ))}
     </ul>
